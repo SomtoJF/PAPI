d94716ef957f810b4bbc85440f64f273820e134e
Francis Somtochukwu <somtofrancis5@gmail.com>
2024-01-31 23:19:43 +0100

    import and use userRouter middleware

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { Express, json } from "express";
 import dotenv from "dotenv";
+import userRouter from "./students/routes";
 
 const app: Express = express();
 dotenv.config();
@@ -9,6 +10,8 @@ const port = process.env.PORT || 8080;
 app.get("/", (req, res) => {
 	res.send("Hello World");
 });
+
+app.use("/api/v1/users", userRouter);
 
 app.listen(port, () => {
 	console.log("listening on port " + port);
